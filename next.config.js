183e0f25c925a5d7012da63b6e4e988a5c967cab
dimasmustaqwa <dimasmustaqwa@gmail.com>
2018-08-26 00:28:31 +0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const routes = require('./routes')
+
+const getRoutes = {
+  exportPathMap: routes
+};
+
+module.exports = {
+  getRoutes
+}
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+export default () => (
+  <div>
+     <p>This is the index page</p>
+  </div>
+)
